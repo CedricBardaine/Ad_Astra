6b4ac4e016f05a7332ad7c55750cf6bf0689eaae
Cédric B <cedricbardaine@gmail.com>
2020-04-05 20:30:39 +0200

    bind les tables Artiste_Talent et Artiste_Need a l'ajout d'un user

--- AdAstra_API/createDB_createTables_fillTheTables_scipt.sql
+++ AdAstra_API/createDB_createTables_fillTheTables_scipt.sql
@@ -248,6 +248,7 @@ INSERT INTO Music (id , id_userStar , id_musical_style ) VALUES (777 , 777 , 777
 
 
 -- Fill tables : for essential rows
+-- -- à ne pas modifier !! certians fct de l'app Ad Astra reposent sur ces ids
 
 INSERT INTO Country (id, name) VALUES (1, "France") ; 
 INSERT INTO Profession (id, name) VALUES (1, "Artiste") ; 
--- AdAstra_app/ad-astra-app/src/app/form-register/form-register.component.ts
+++ AdAstra_app/ad-astra-app/src/app/form-register/form-register.component.ts
@@ -84,6 +84,8 @@ export class FormRegisterComponent implements OnInit {
   
   ngOnInit() {
     this.theFormState = FORMSTATE.question_1;
+    
+    // inits for the checkboxes 
     this.theNewUser = {} ; 
     this.theNewUser.talents = {} ; 
     this.theNewUser.genres = {} ; 
@@ -92,27 +94,316 @@ export class FormRegisterComponent implements OnInit {
     this.theNewUser.infos = {} ; 
   }  
   
-  
+  /**
+  * Send the form data to the API, adding rows in the good tables and binding them. 
+  * // TODO: add a case when there is a problem
+  * // FIXME: bind and add row to the good tables in the DB, in order to have a clean user 
+  */
   onSubmit() {
-    let postData = 
+    let dateSuscribe = this.getcurrentDate("yyyy","mm","dd", "-"); 
+    
+    let postDataUserStar = 
     {
-      id_userStar: 777,
+      firstname:this.theNewUser.infos.prenom,
+      lastname:this.theNewUser.infos.nom,
+      // birth:2020-03-13 // FIXME: obtain actual date of the new user, not only a yes no
+      mail:this.theNewUser.infos.email, 
+      password:this.theNewUser.infos.mdp, // FIXME: find a way to have it encrypted
+      sign_in_date:dateSuscribe,
+      // photo:42
+      // name_spotify:
+      // name_deezer:
+      // name_youtube:
+      // name_bandcamp:
+      id_profession:1, // 1 : "Artiste"
+      id_country:1 // 1 : "France"
+    };
+    let postDataSurvey = 
+    {
+      content:"?..." // TODO: decide what to do with this table
+    }
+    let postDataArtist = 
+    {
+      id_userStar: 0,
       artist_name:this.theNewUser.infos.nomArtiste,
-      suscribed_until:"2020-07-25",
+      // suscribed_until:"2020-07-25",
       suscribed:false,
-      id_survey:777,
+      id_survey:0,
       checked:false
     };
-    let url = 'http://localhost:5000/insert_artist';
+    
+    // let urlToCountry = "http://localhost:5000/insert_Country";
+    // let urlToProfession = "http://localhost:5000/insert_Profession";
+    // !!!! ils sont déjà de base faut juste binder dans la table UserStar 
+    let urlToUserStar = 'http://localhost:5000/insert_UserStar';
+    let urlToSurvey = 'http://localhost:5000/insert_Survey';
+    let urlToArtist = 'http://localhost:5000/insert_artist';
+    // let urlToArtistTalent = "http://localhost:5000/insert_Artist_Talent";
+    let urlToArtistNeed = "http://localhost:5000/insert_Artist_Need";
     // let url = "http://httpbin.org/post"; // !!!! test url that returns the post datas
     
-    this.http.post(url, postData).toPromise().then(data => {
-      this.debugLabel = "Nouveau user enregistré ! " + JSON.stringify(data) ; 
-      this.theStep2 = false ; 
-      this.theFormState = 9 ; 
-      // TODO: add a case when there is a problem
-      // FIXME: bind and add row to the good tables in the DB, in order to have a clean user 
+    let returnOfToUserStar ;
+    let returnOfToSurvey ;
+    let returnOfToArtist ;
+    // let returnOfToArtistTalent ;
+    // let returnOfToArtistNeed ;
+    
+    this.http.post(urlToUserStar, postDataUserStar).toPromise().then(dataUserStar => {
+      returnOfToUserStar = dataUserStar ; 
+      
+      if ( returnOfToUserStar.insertId) {
+        this.http.post(urlToSurvey, postDataSurvey).toPromise().then(dataSurvey => {
+          returnOfToSurvey = dataSurvey ; 
+          
+          if ( returnOfToSurvey.insertId) { 
+            postDataArtist.id_userStar = returnOfToUserStar.insertId ; 
+            postDataArtist.id_survey = returnOfToSurvey.insertId ; 
+            
+            this.http.post(urlToArtist, postDataArtist).toPromise().then(dataArtist => {
+              returnOfToArtist = dataArtist ; 
+              
+              if ( returnOfToArtist.insertId) {
+                this.debugLabel = this.postDataToArtistTalent(returnOfToArtist.insertId) ; 
+                this.debugLabel += this.postDataToArtisteNeed(returnOfToArtist.insertId) ; 
+              }
+              
+              //TODO: continuer en bindant avec le userstar musical style 
+              
+              // FIXME: terminer :  en fin d'insertion de tout on  vérifie :   tout est bon ? on désafiche le form : on annule (delete) tout et on affiche une erreur 
+              if ( returnOfToArtist.insertId ) {
+                this.debugLabel = "Nouveau user enregistré ! " + JSON.stringify(dataArtist) ; 
+                this.theStep2 = false ; 
+                this.theFormState = 9 ; 
+              }
+            });
+          }
+          
+        });
+        
+      }
     });
+    
+    
+    
+    // this.http.post(urlToArtist, postDataArtist).toPromise().then(data => {
+    //   this.debugLabel = "Nouveau user enregistré ! " + JSON.stringify(data) ; 
+    //   this.theStep2 = false ; 
+    //   this.theFormState = 9 ; 
+    // });
   }
+  
+  /**
+  * Send post request to add rows in Artist_Talent table for each talents of this.theNewUser .
+  * @param idArtist 
+  * @return all the responses of API
+  */
+  postDataToArtistTalent(idArtist) {
+    let postDataArtistTalent = {
+      id_artist:0,
+      id_talent:0
+    };
+    let urlToArtistTalent = "http://localhost:5000/insert_Artist_Talent";
+    let ret = "{" ;
+    
+    postDataArtistTalent.id_artist = idArtist ; 
+    let talents = this.theNewUser.talents ; 
+    for ( var key in talents) {
+      // console.log(key , " - " , talents[key]) ; 
+      switch (key) {
+        case "compositeur" :
+        postDataArtistTalent.id_talent = 1 ;
+        this.http.post( urlToArtistTalent , postDataArtistTalent).toPromise().then(dataArtistTalent => {
+          ret += dataArtistTalent + ","; 
+        }); 
+        break;
+        case "parolier" :
+        postDataArtistTalent.id_talent = 2 ;
+        this.http.post( urlToArtistTalent , postDataArtistTalent).toPromise().then(dataArtistTalent => {
+          ret += dataArtistTalent + ","; 
+        }); 
+        break;
+        case "arrangeur" :
+        postDataArtistTalent.id_talent = 3 ;
+        this.http.post( urlToArtistTalent , postDataArtistTalent).toPromise().then(dataArtistTalent => {
+          ret += dataArtistTalent + ","; 
+        }); 
+        break;
+        case "auteurDeChansons" :
+        postDataArtistTalent.id_talent = 4 ;
+        this.http.post( urlToArtistTalent , postDataArtistTalent).toPromise().then(dataArtistTalent => {
+          ret += dataArtistTalent + ","; 
+        }); 
+        break;
+        case "chanteur" :
+        postDataArtistTalent.id_talent = 5 ;
+        this.http.post( urlToArtistTalent , postDataArtistTalent).toPromise().then(dataArtistTalent => {
+          ret += dataArtistTalent + ","; 
+        }); 
+        break;
+        case "musicienDeScene" :
+        postDataArtistTalent.id_talent = 6 ;
+        this.http.post( urlToArtistTalent , postDataArtistTalent).toPromise().then(dataArtistTalent => {
+          ret += dataArtistTalent + ","; 
+        }); 
+        break;
+        case "musicienDeStudio" :
+        postDataArtistTalent.id_talent = 7 ;
+        this.http.post( urlToArtistTalent , postDataArtistTalent).toPromise().then(dataArtistTalent => {
+          ret += dataArtistTalent + ","; 
+        }); 
+        break;
+        case "dj" :
+        postDataArtistTalent.id_talent = 8 ;
+        this.http.post( urlToArtistTalent , postDataArtistTalent).toPromise().then(dataArtistTalent => {
+          ret += dataArtistTalent + ","; 
+        }); 
+        break;
+        default :
+        this.debugLabel = "ERROR! in posting ArtistTalent"; 
+        break;
+      }
+      ret += "}" ; // FIXME: return ce string ; "{}}" donc voir si ça peut s'arranger avec   JSON.stringify(data)  par ex. 
+    }
+    
+    return ret ; 
+  }
+  
+  /**
+  * Send post request to add rows in Artist_Talent table for each talents of this.theNewUser .
+  * @param idArtist 
+  * @return all the responses of API
+  */
+ postDataToArtisteNeed(idArtist) {
+  let postDataArtistNeed = 
+  {
+    id_artist:0,
+    id_need:0
+  };
+  let urlToArtistTalent = "http://localhost:5000/insert_Artist_Need";
+  let ret = "{" ;
 
+  postDataArtistNeed.id_artist = idArtist ; 
+  let services = this.theNewUser.services ; 
+  for ( var key in services) {
+    // console.log(key , " - " , services[key]) ; 
+    switch (key) {
+      case "aideManagement" :
+      postDataArtistNeed.id_need = 1 ;
+      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+        ret += data + ","; 
+      }); 
+      break;
+      case "gestionDroits" :
+      postDataArtistNeed.id_need = 2 ;
+      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+        ret += data + ","; 
+      }); 
+      break;
+      case "obtenirVisibilite" :
+      postDataArtistNeed.id_need = 3 ;
+      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+        ret += data + ","; 
+      }); 
+      break;
+      case "devenirIntermittent" :
+      postDataArtistNeed.id_need = 4 ;
+      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+        ret += data + ","; 
+      }); 
+      break;
+      case "mieuxConnaitre" :
+      postDataArtistNeed.id_need = 5 ;
+      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+        ret += data + ","; 
+      }); 
+      break;
+      case "chercherSubvention" :
+      postDataArtistNeed.id_need = 6 ;
+      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+        ret += data + ","; 
+      }); 
+      break;
+      case "faireReseau" :
+      postDataArtistNeed.id_need = 7 ;
+      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+        ret += data + ","; 
+      }); 
+      break;
+      default :
+      this.debugLabel = "ERROR! in posting ArtistTalent"; 
+      break;
+    }
+    ret += "}" ; // FIXME: return ce string ; "{}}" donc voir si ça peut s'arranger avec   JSON.stringify(data)  par ex. 
+  }
+  
+  return ret ; 
+}
+
+  
+  
+  
+  
+  
+  
+  /**
+  * @param unit1 either "dd" or "mm" or "yyyy" (day, month, year).
+  * @param unit2 either "dd" or "mm" or "yyyy" (day, month, year).
+  * @param unit3 either "dd" or "mm" or "yyyy" (day, month, year).
+  * @param separator the symbol inserted between values.
+  * @return the current date as the gived format.
+  */
+  getcurrentDate(unit1:string,  unit2:string,  unit3:string, separator:string) {
+    let ret:string = "" ;
+    
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    
+    switch (unit1) {
+      case "dd": 
+      ret += dd ;
+      break;
+      case "mm": 
+      ret += mm ;
+      break;
+      case "yyyy": 
+      ret += yyyy ;
+      break;
+    }
+    
+    ret += separator ; 
+    
+    switch (unit2) {
+      case "dd": 
+      ret += dd ;
+      break;
+      case "mm": 
+      ret += mm ;
+      break;
+      case "yyyy": 
+      ret += yyyy ;
+      break;
+    }
+    
+    ret += separator ; 
+    
+    switch (unit3) {
+      case "dd": 
+      ret += dd ;
+      break;
+      case "mm": 
+      ret += mm ;
+      break;
+      case "yyyy": 
+      ret += yyyy ;
+      break;
+    }
+    
+    return ret ; 
+  }
+  
+  
+  
+  
 }
