2d938c562359fbc8513c69eea429ebbf09f28289
Cédric B <cedricbardaine@gmail.com>
2020-04-10 17:53:38 +0200

    add security and in-form message for ergonomie in register form

--- AdAstra_app/ad-astra-app/src/app/form-register/form-register.component.ts
+++ AdAstra_app/ad-astra-app/src/app/form-register/form-register.component.ts
@@ -75,6 +75,8 @@ export class FormRegisterComponent implements OnInit {
   debugLabel = "" ; // FIXME: FM7 delete before prod
   theFormState: FORMSTATE ; 
   theStep2 = false ;
+  confirmPassWord = "";
+  errorInRegisterForm = "";
   
   theNewUser: NewUser ;
   
@@ -94,12 +96,12 @@ export class FormRegisterComponent implements OnInit {
     this.theNewUser.relationsVoulues = {} ; 
     this.theNewUser.infos = {} ; 
   }  
-
+  
   /**
-   * Verify if the NewUser is older than 16. 
-   * If yes : the form continue.
-   * If no : the registration stops. 
-   */
+  * Verify if the NewUser is older than 16. 
+  * If yes : the form continue.
+  * If no : the registration stops. 
+  */
   birthDateValidation() {
     let uBDate = new Date ( this.theNewUser.infos.date ) ;
     let tooYoung:boolean;
@@ -107,7 +109,7 @@ export class FormRegisterComponent implements OnInit {
     let minYear = today.getFullYear()-16;
     let minMonth = today.getMonth();
     let minDay = today.getDate();
-
+    
     if ( uBDate.getFullYear() < minYear ) tooYoung = false; 
     else if (uBDate.getFullYear() > minYear) tooYoung = true; 
     else if (uBDate.getFullYear() == minYear) {
@@ -122,7 +124,7 @@ export class FormRegisterComponent implements OnInit {
         }
       }
     }
-
+    
     if (tooYoung) this.theFormState = -3 ; 
     else this.theFormState = 4 ; 
   }
@@ -133,85 +135,127 @@ export class FormRegisterComponent implements OnInit {
   * // FIXME: bind and add row to the good tables in the DB, in order to have a clean user 
   */
   onSubmit() {
-    //TODO:  this.customFormValidation() ; 
-    let dateSuscribe = this.getcurrentDate("yyyy","mm","dd", "-"); 
-    
-    let postDataUserStar = 
-    {
-      firstname:this.theNewUser.infos.prenom,
-      lastname:this.theNewUser.infos.nom,
-      // birth:2020-03-13 // FIXME: obtain actual date of the new user, not only a yes no
-      mail:this.theNewUser.infos.email, 
-      password:this.theNewUser.infos.mdp, // FIXME: find a way to have it encrypted
-      sign_in_date:dateSuscribe,
-      id_profession:1,  // 1 : "Artiste"
-      id_country:1      // 1 : "France"
-    };
-    let postDataSurvey = 
-    {
-      content:"?..." // TODO: decide what to do with this table
-    }
-    let postDataArtist = 
-    {
-      id_userStar: 0,
-      artist_name:this.theNewUser.infos.nomArtiste,
-      // suscribed_until:"2020-07-25",
-      suscribed:false,
-      id_survey:0,
-      checked:false
-    };
-    
-    let urlToUserStar = 'http://localhost:5000/insert_UserStar';
-    let urlToSurvey = 'http://localhost:5000/insert_Survey';
-    let urlToArtist = 'http://localhost:5000/insert_artist';
-    
-    let returnOfToUserStar ;
-    let returnOfToSurvey ;
-    let returnOfToArtist ;
-    // let returnOfToArtistTalent ;
-    // let returnOfToArtistNeed ;
-    
-    this.http.post(urlToUserStar, postDataUserStar).toPromise().then(dataUserStar => {
-      returnOfToUserStar = dataUserStar ; 
+    if ( this.customFormValidation() ) {
       
-      if ( returnOfToUserStar.insertId) {
-        this.http.post(urlToSurvey, postDataSurvey).toPromise().then(dataSurvey => {
-          returnOfToSurvey = dataSurvey ; 
-          
-          if ( returnOfToSurvey.insertId) { 
-            postDataArtist.id_userStar = returnOfToUserStar.insertId ; 
-            postDataArtist.id_survey = returnOfToSurvey.insertId ; 
-            
-            this.http.post(urlToArtist, postDataArtist).toPromise().then(dataArtist => {
-              returnOfToArtist = dataArtist ; 
-              
-              if ( returnOfToArtist.insertId) {
-                this.debugLabel = this.postDataToArtistTalent(returnOfToArtist.insertId) ; 
-                this.debugLabel += this.postDataToArtisteNeed(returnOfToArtist.insertId) ; 
-              }
-              
-              // FIXME: terminer :  en fin d'insertion de tout on  vérifie :   tout est bon ? on désafiche le form : on annule (delete) tout et on affiche une erreur  parceque le mettre ici c'est un peu borderline niveau propreté de code.....
-              // Utiliser pour cela les returnOfToUserStar et supprimer les dernières insertions correspondantes
-              if ( returnOfToArtist.insertId ) {
-                this.debugLabel = "Nouveau user enregistré ! " + JSON.stringify(dataArtist) ; 
-                this.theStep2 = false ; 
-                this.theFormState = 9 ; 
-              }
-            });
-          }
-          
-        });
-
-        this.postDataToUserStar_MusicalStyle(returnOfToUserStar.insertId) ; 
-        
+      let dateSuscribe = this.getcurrentDate("yyyy","mm","dd", "-"); 
+      
+      let postDataUserStar = 
+      {
+        firstname:this.theNewUser.infos.prenom,
+        lastname:this.theNewUser.infos.nom,
+        // birth:2020-03-13 // FIXME: obtain actual date of the new user, not only a yes no
+        mail:this.theNewUser.infos.email, 
+        password:this.theNewUser.infos.mdp, // FIXME: find a way to have it encrypted
+        sign_in_date:dateSuscribe,
+        id_profession:1,  // 1 : "Artiste"
+        id_country:1      // 1 : "France"
+      };
+      let postDataSurvey = 
+      {
+        content:"?..." // TODO: decide what to do with this table
       }
-    });
-    
+      let postDataArtist = 
+      {
+        id_userStar: 0,
+        artist_name:this.theNewUser.infos.nomArtiste,
+        // suscribed_until:"2020-07-25",
+        suscribed:false,
+        id_survey:0,
+        checked:false
+      };
+      
+      let urlToUserStar = 'http://localhost:5000/insert_UserStar';
+      let urlToSurvey = 'http://localhost:5000/insert_Survey';
+      let urlToArtist = 'http://localhost:5000/insert_artist';
+      
+      let returnOfToUserStar ;
+      let returnOfToSurvey ;
+      let returnOfToArtist ;
+      // let returnOfToArtistTalent ;
+      // let returnOfToArtistNeed ;
+      
+      this.http.post(urlToUserStar, postDataUserStar).toPromise().then(dataUserStar => {
+        returnOfToUserStar = dataUserStar ; 
+        
+        if ( returnOfToUserStar.insertId) {
+          this.http.post(urlToSurvey, postDataSurvey).toPromise().then(dataSurvey => {
+            returnOfToSurvey = dataSurvey ; 
+            
+            if ( returnOfToSurvey.insertId) { 
+              postDataArtist.id_userStar = returnOfToUserStar.insertId ; 
+              postDataArtist.id_survey = returnOfToSurvey.insertId ; 
+              
+              this.http.post(urlToArtist, postDataArtist).toPromise().then(dataArtist => {
+                returnOfToArtist = dataArtist ; 
+                
+                if ( returnOfToArtist.insertId) {
+                  this.debugLabel = this.postDataToArtistTalent(returnOfToArtist.insertId) ; 
+                  this.debugLabel += this.postDataToArtisteNeed(returnOfToArtist.insertId) ; 
+                }
+                
+                // FIXME: terminer :  en fin d'insertion de tout on  vérifie :   tout est bon ? on désafiche le form : on annule (delete) tout et on affiche une erreur  parceque le mettre ici c'est un peu borderline niveau propreté de code.....
+                // Utiliser pour cela les returnOfToUserStar et supprimer les dernières insertions correspondantes
+                if ( returnOfToArtist.insertId ) {
+                  this.debugLabel = "Nouveau user enregistré ! " + JSON.stringify(dataArtist) ; 
+                  this.theStep2 = false ; 
+                  this.theFormState = 9 ; 
+                }
+              });
+            }
+            
+          });
+          
+          this.postDataToUserStar_MusicalStyle(returnOfToUserStar.insertId) ; 
+          
+        }
+      });
+    }
+    else { this.errorInRegisterForm += " !"; }
   }
-
+  
+  /**
+  * @return true if the values of the form (both part1 and 2) are correct 
+  * and good to be inserted in DB. 
+  * //TODO: verif names length
+  * //TODO: verif if at least one checkboxe by categories is checked
+  */
   customFormValidation() {
-    throw new Error("Method not implemented.");
+    let ret:boolean = false; 
+    // throw new Error("Method not implemented.");
+    
+    if (this.theNewUser.infos.nom && this.theNewUser.infos.prenom) {
+      ret = true;
+
+      if ( this.validateEmail(this.theNewUser.infos.email) ) {
+        ret = true;
+        
+        if ( this.theNewUser.infos.mdp.toString() == this.confirmPassWord.toString() ) {
+          ret = true;
+        }
+
+        else {
+          ret = false; 
+          this.errorInRegisterForm = "Attention, les mots de passes ne correspondent pas"
+        }
+      } 
+      else {
+        ret = false; 
+        this.errorInRegisterForm = "L'addresse email n'est pas correcte"; 
+      } 
+    }
+    else {
+      ret = false; 
+      this.errorInRegisterForm = "Veuillez rentrer votre nom et votre prénom";
+    }
+    
+    
+    return ret; 
   }
+  validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ;
+    return re.test(String(email).toLowerCase());
+  }
+  
   
   /**
   * Send post request to add rows in Artist_Talent table for each talents of this.theNewUser .
@@ -294,148 +338,148 @@ export class FormRegisterComponent implements OnInit {
   * @param idArtist 
   * @return all the responses of API
   */
- postDataToArtisteNeed(idArtist) {
-  let postDataArtistNeed = 
-  {
-    id_artist:0,
-    id_need:0
-  };
-  let urlToArtistTalent = "http://localhost:5000/insert_Artist_Need";
-  let ret = "{" ;
-
-  postDataArtistNeed.id_artist = idArtist ; 
-  let services = this.theNewUser.services ; 
-  for ( var key in services) {
-    // console.log(key , " - " , services[key]) ; 
-    switch (key) {
-      case "aideManagement" :
-      postDataArtistNeed.id_need = 1 ;
-      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "gestionDroits" :
-      postDataArtistNeed.id_need = 2 ;
-      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "obtenirVisibilite" :
-      postDataArtistNeed.id_need = 3 ;
-      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "devenirIntermittent" :
-      postDataArtistNeed.id_need = 4 ;
-      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "mieuxConnaitre" :
-      postDataArtistNeed.id_need = 5 ;
-      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "chercherSubvention" :
-      postDataArtistNeed.id_need = 6 ;
-      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "faireReseau" :
-      postDataArtistNeed.id_need = 7 ;
-      this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      default :
-      this.debugLabel = "ERROR! in posting ArtistTalent"; 
-      break;
+  postDataToArtisteNeed(idArtist) {
+    let postDataArtistNeed = 
+    {
+      id_artist:0,
+      id_need:0
+    };
+    let urlToArtistTalent = "http://localhost:5000/insert_Artist_Need";
+    let ret = "{" ;
+    
+    postDataArtistNeed.id_artist = idArtist ; 
+    let services = this.theNewUser.services ; 
+    for ( var key in services) {
+      // console.log(key , " - " , services[key]) ; 
+      switch (key) {
+        case "aideManagement" :
+        postDataArtistNeed.id_need = 1 ;
+        this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "gestionDroits" :
+        postDataArtistNeed.id_need = 2 ;
+        this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "obtenirVisibilite" :
+        postDataArtistNeed.id_need = 3 ;
+        this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "devenirIntermittent" :
+        postDataArtistNeed.id_need = 4 ;
+        this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "mieuxConnaitre" :
+        postDataArtistNeed.id_need = 5 ;
+        this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "chercherSubvention" :
+        postDataArtistNeed.id_need = 6 ;
+        this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "faireReseau" :
+        postDataArtistNeed.id_need = 7 ;
+        this.http.post( urlToArtistTalent , postDataArtistNeed).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        default :
+        this.debugLabel = "ERROR! in posting ArtistTalent"; 
+        break;
+      }
+      ret += "}" ; // FIXME: FM306 
     }
-    ret += "}" ; // FIXME: FM306 
+    
+    return ret ; 
   }
   
-  return ret ; 
-}
-
   /**
   * Send post request to add rows in UserStar_MusicalStyle table for each genres of this.theNewUser .
   * @param idArtist 
   * @return all the responses of API
   */
- postDataToUserStar_MusicalStyle(idArtist) {
-  let postData_USMS = 
-  {
-    id_userStar:0,
-    id_Musical_style:0
-  };
-  let url_USMS = "http://localhost:5000/insert_UserStar_Musical_style";
-  let ret = "{" ;
-
-  postData_USMS.id_userStar = idArtist ; 
-  let genres = this.theNewUser.genres ; 
-  for ( var key in genres) {
-    switch (key) {
-      case "blues" :
-      postData_USMS.id_Musical_style = 1 ;
-      this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "funk" :
-      postData_USMS.id_Musical_style = 2 ;
-      this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "jazz" :
-      postData_USMS.id_Musical_style = 3 ;
-      this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "metal" :
-      postData_USMS.id_Musical_style = 4 ;
-      this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "pop" :
-      postData_USMS.id_Musical_style = 5 ;
-      this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "punk" :
-      postData_USMS.id_Musical_style = 6 ;
-      this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "rap" :
-      postData_USMS.id_Musical_style = 7 ;
-      this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      case "rockNRoll" :
-      postData_USMS.id_Musical_style = 8 ;
-      this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
-        ret += data + ","; 
-      }); 
-      break;
-      default :
-      this.debugLabel = "ERROR! in posting ArtistTalent"; 
-      break;
+  postDataToUserStar_MusicalStyle(idArtist) {
+    let postData_USMS = 
+    {
+      id_userStar:0,
+      id_Musical_style:0
+    };
+    let url_USMS = "http://localhost:5000/insert_UserStar_Musical_style";
+    let ret = "{" ;
+    
+    postData_USMS.id_userStar = idArtist ; 
+    let genres = this.theNewUser.genres ; 
+    for ( var key in genres) {
+      switch (key) {
+        case "blues" :
+        postData_USMS.id_Musical_style = 1 ;
+        this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "funk" :
+        postData_USMS.id_Musical_style = 2 ;
+        this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "jazz" :
+        postData_USMS.id_Musical_style = 3 ;
+        this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "metal" :
+        postData_USMS.id_Musical_style = 4 ;
+        this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "pop" :
+        postData_USMS.id_Musical_style = 5 ;
+        this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "punk" :
+        postData_USMS.id_Musical_style = 6 ;
+        this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "rap" :
+        postData_USMS.id_Musical_style = 7 ;
+        this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        case "rockNRoll" :
+        postData_USMS.id_Musical_style = 8 ;
+        this.http.post( url_USMS , postData_USMS).toPromise().then(data => {
+          ret += data + ","; 
+        }); 
+        break;
+        default :
+        this.debugLabel = "ERROR! in posting ArtistTalent"; 
+        break;
+      }
+      ret += "}" ; // FIXME: FM306
     }
-    ret += "}" ; // FIXME: FM306
+    
+    return ret ; 
   }
   
-  return ret ; 
-}
-
   
   
   
