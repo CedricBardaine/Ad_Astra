78feeeb64dd5a81e5deb9616a391f5fec61f9085
Cédric B <cedricbardaine@gmail.com>
2020-05-03 16:35:33 +0200

    create & init contactPage

--- AdAstra_app/ad-astra-app/src/app/app.module.ts
+++ AdAstra_app/ad-astra-app/src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormRegisterComponent } from './form-register/form-register.component';
 
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpClientModule }    from '@angular/common/http';
 
 // routing
 import { RouterModule , Routes } from '@angular/router';
+
+// pages
+import { FormRegisterComponent } from './form-register/form-register.component';
 import { Page404Component } from './page404/page404.component';
 import { PageArtistComponent } from './page-artist/page-artist.component';
+import { PageContactComponent } from './page-contact/page-contact.component';
 
 // styles compoents from F
 import { ButtonComponent } from './style-components/button/button.component';
@@ -25,6 +28,7 @@ const appRoutes: Routes = [
   { path: '404', component: Page404Component },
   { path: 'artist', component: PageArtistComponent },
   { path: 'artist/:id', component: PageArtistComponent },
+  { path: 'contact', component: PageContactComponent },
   
   { path: '', component: FormRegisterComponent },
   { path: '**', component: Page404Component },
@@ -41,6 +45,7 @@ const appRoutes: Routes = [
     HeaderComponent,
     ButtonRegisterComponent,
     HeaderLoggedInComponent,
+    PageContactComponent,
   ],
   imports: [
     BrowserModule,
