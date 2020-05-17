5a5385b556ee0378431a14c504e5d27268228fab
Cédric B <cedricbardaine@gmail.com>
2020-05-17 10:52:31 +0200

    change tag_variable name

--- AdAstra_API/Actions/Picture.js
+++ AdAstra_API/Actions/Picture.js
@@ -12,7 +12,7 @@ module.exports = {
     * Method with the "formidable" library. 
     */
     insert_Picture: (req, res) => {
-        const insertPict = "insertPicture: "
+        const TAG_insertPict = "insertPicture: "
 
         // // let id = req.body.id; 
         let id_userStar;
@@ -24,16 +24,16 @@ module.exports = {
         let errorFieldCount = 1 ; 
         
         form.on('error', function (err) {
-            console.log(insertPict+ "error : "+err);
+            console.log(TAG_insertPict+ "error : "+err);
             res.writeHead(200, {'content-type': 'text/plain'});
             res.end('error:\n\n'+util.inspect(err));
         })
-        .on('aborted', function (err) { console.log(insertPict+ "user aborted upload : "+err); })
-        .on('end', function() { console.log(insertPict+ 'End of operation.'); })
+        .on('aborted', function (err) { console.log(TAG_insertPict+ "user aborted upload : "+err); })
+        .on('end', function() { console.log(TAG_insertPict+ 'End of operation.'); })
         .on('field', (fieldName, fieldValue) => {
-            if (fieldName != "id_userStar") console.log(insertPict+ "Error ! Wrong form field sent ! n°"+ (errorFieldCount++) );
+            if (fieldName != "id_userStar") console.log(TAG_insertPict+ "Error ! Wrong form field sent ! n°"+ (errorFieldCount++) );
             else {
-                console.log(insertPict+ fieldName + " : "+ fieldValue);
+                console.log(TAG_insertPict+ fieldName + " : "+ fieldValue);
                 id_userStar = fieldValue ; 
                 
                 let query = "INSERT INTO `Picture` (id_userStar) VALUES ('" +
@@ -44,8 +44,8 @@ module.exports = {
                     let imagePathAndName = form.uploadDir + result.insertId;
                     
                     form.on('file', function (field, file) {
-                        //   console.log(insertPict+ "field : "+field, file);
-                        console.log(insertPict+ "saving file...");
+                        //   console.log(TAG_insertPict+ "field : "+field, file);
+                        console.log(TAG_insertPict+ "saving file...");
                         // file.push([field, file]); // 15/05/2020 : doesn't exist ? 
                         // fs.rename(file.path, form.uploadDir + "/" + "9999.jpg", function( error ) {});
                         let fileType = file.type.split("/")[1];
@@ -55,8 +55,8 @@ module.exports = {
                     
                     form.parse(req, (err, fields, files) => {
                         // res.writeHead(200, { 'content-type': 'application/json' });
-                        // console.log(insertPict+ JSON.stringify({ fields, files }, null, 2));
-                        console.log(insertPict+ "saved picture to : "+imagePathAndName);
+                        // console.log(TAG_insertPict+ JSON.stringify({ fields, files }, null, 2));
+                        console.log(TAG_insertPict+ "saved picture to : "+imagePathAndName);
                     });
                     
                     res.send(result) ;            
